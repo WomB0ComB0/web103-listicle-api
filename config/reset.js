5ae1ac81c250c2a1c2efaab76fe6aee9f41e2461
Mike Odnis <95197809+WomB0ComB0@users.noreply.github.com>
2024-02-04 23:07:17 -0500

    Quick commit

--- config/reset.js
+++ config/reset.js
@@ -4,8 +4,6 @@ import topMovies from '../data/data.js';
 
 const createMoviesTable = async () => {
   const createTableQuery = `
-    DROP TABLE IF EXISTS topmovies;
-
     CREATE TABLE IF NOT EXISTS topmovies (
       id SERIAL PRIMARY KEY,
       title VARCHAR(255) NOT NULL,
